525c6d7879c52672bbd91f7576d9f1950d7a0a90
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-09-06 15:05:25 +0200

    add materialise

--- formulaire.php
+++ formulaire.php
@@ -1,6 +1,10 @@
+
+<div class="row ">
 <h1>Connexion</h1>
-<form action="index.php" method="post">
-<label for=""></label>
-<input type="password" name="password_user" placeholder="#simplon!">
-<input type="submit" name="" value="Envoyer">
+
+<form action="index.php" method="post" class="input-field col s6 card blue-grey darken-1">
+<label for="1">password</label>
+<input id="1" type="password" name="password_user" placeholder="#simplon!">
+<input class="waves-effect waves-light btn " type="submit" name="" value="Envoyer">
 </form>
+</div>
--- index.php
+++ index.php
@@ -3,13 +3,14 @@
   <head>
     <meta charset="utf-8">
     <title>Module de connexion</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
   </head>
   <body>
 
 <?php
 $password = "#simplon!";
 
-if (!isset($_POST['password_user'])) {
+if (!isset($_POST['password_user']) OR strlen($_POST['password_user'])==0) {
   include("formulaire.php");
 echo "<p>
 Veuillez saisir un mot de passe !
@@ -31,5 +32,7 @@ Le mot de passe pour le bombe atomique est : 3456 7898 9987 4436
 };
 
  ?>
+ <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
   </body>
 </html>
