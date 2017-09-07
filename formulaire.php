5b937f9c43d108f76202b2b41e716c589d5ed107
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-09-07 09:15:56 +0200

    add style in formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -1,10 +1,15 @@
 
-<div class="row ">
-<h1>Connexion</h1>
+<div class="row">
 
-<form action="index.php" method="post" class="input-field col s6 card blue-grey darken-1">
+
+
+<div id="formulaire" class="col s6 card blue-grey">
+<h1 class="card-title ">Connexion</h1>
+
+<form action="index.php" method="post" class="input-field ">
 <label for="1">password</label>
 <input id="1" type="password" name="password_user" placeholder="#simplon!">
-<input class="waves-effect waves-light btn " type="submit" name="" value="Envoyer">
+<input id="envoyer" class="waves-effect waves-light btn " type="submit" name="" value="Envoyer">
 </form>
+</div>
 </div>
